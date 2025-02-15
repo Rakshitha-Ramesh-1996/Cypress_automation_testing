077f32360e6af187b43d51ff37b6c2ff9f4d0c1e
Rakshitha-Ramesh-1996 <browserdata2024@gmail.com>
2025-02-15 18:32:08 -0500

    Ignore node_modules

---
+++ cypress/e2e/MyFirstTest.cy.js
@@ -0,0 +1,11 @@
+describe('My First Test', () => {
+    it('Verify title-positive', () => {
+cy.visit("https://opensource-demo.orangehrmlive.com/")
+      cy.title().should('eq','OrangeHRM')
+    })
+
+    it('Verify title-negative test', () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+              cy.title().should('eq','OrangeHRM123')
+            })
+  })
